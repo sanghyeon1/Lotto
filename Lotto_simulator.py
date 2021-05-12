d4ffe037ded370b54bce46c8fba867b7637df9c8
sanghyeon1 <53289616+sanghyeon1@users.noreply.github.com>
2021-05-12 21:26:04 +0900

    fix the bug
     fix the bug what can't input the number in variable 'n'
    line 95 -> ent2 = Entry(win)
    line 96 -> ent2.pack()
    line 54 -> puts = ent2.get()

---
+++ Lotto_simulator.py
@@ -0,0 +1,103 @@
+from tkinter import *
+import requests
+import random
+from bs4 import BeautifulSoup
+
+
+def lotto_p():
+    n = ent.get()
+    url = "https://www.dhlottery.co.kr/gameResult.do?method=byWin&drwNo={}".format(n)
+    #  div class="win_result"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+    txt = soup.find("div", attrs={"class", "win_result"}).get_text()
+    num_list = txt.split("\n")[7:13]
+    bonus = txt.split("\n")[-4]
+
+    print("당첨번호 - {}".format(num_list))
+    print("보너스 번호 - {}".format(bonus))
+
+
+def prd_lotto():  # 로또 번호 예측 코드
+    count_num = []
+    min_list = []
+    bns_sum = 0
+    for i in range(1, 46):
+        count_num.append(0)
+
+    # 랜덤 범위 설정 후 로또번호 예측
+    b = random.randint(101, 962)
+    a = b - 50
+    for i in range(a, b):
+        pre_url = "https://www.dhlottery.co.kr/gameResult.do?method=byWin&drwNo={}".format(i)
+        req = requests.get(pre_url)
+        soup = BeautifulSoup(req.text, "html.parser")
+        txt = soup.find("div", attrs={"class", "win_result"}).get_text()
+        lotto_num = txt.split("\n")[7:13]
+        bns_num = txt.split("\n")[-4]
+        for j in range(6):
+            count_num[int(lotto_num[j]) - 1] += 1
+            # 보너스 번호를 계산하는데에 한 번만 쓰입니다.
+            if j == 0:
+                count_num[int(bns_num) - 1] += 1
+
+    for i in range(7):
+        # count_num 의 최솟값에 해당하는 인덱스가 tmp 에 들어감.
+        tmp = count_num.index(min(count_num))
+        min_list.append(tmp+1)
+        count_num[tmp] = 100
+
+    print(min_list[0:6], "+ 보너스 번호 [{}]".format(min_list[6]))
+
+
+def count_prd():
+    puts = ent2.get()
+    if type(puts) == str and puts == '':
+        print("아무것도 입력되지 않았습니다.")
+
+    elif puts != '':
+        puts = int(puts)
+        if 0 < puts < 11:
+            print("로또 번호를 {}회 뽑습니다.".format(puts))
+            print("{}초 정도 소요됩니다...".format(puts * 21))
+            for i in range(puts):
+                if i > 0:
+                    print("\n{}초 정도 남았습니다...".format((puts-i)*21))
+                print("{}회 : ".format(i+1), end='')
+                prd_lotto()
+            print("완료되었습니다.")
+        elif puts >= 11:
+            print("입력 값이 너무 커서 프로그램이 느려집니다.")
+            print("1~10사이의 값을 입력하십시오.")
+        else:
+            print("Error 값입니다. 다시 입력하십시오.")
+
+
+win = Tk()
+win.geometry("300x200")
+win.option_add("*Font", "궁서 20")
+
+ent = Entry(win)
+ent.pack()
+
+
+def ent_p():
+    a = ent.get()
+    print(a)
+
+
+# 일반 로또 당첨번호 확인 버튼
+btn = Button(win)
+btn.config(text="로또 당첨 번호 확인")
+btn.config(command=lotto_p)
+btn.pack()
+
+ent2 = Entry(win)
+ent2.pack()
+
+# 당첨번호 예측하기 버튼
+btn2 = Button(win)
+btn2.config(text="당첨번호 예측하기")
+btn2.config(command=count_prd)
+btn2.pack()
+win.mainloop()
